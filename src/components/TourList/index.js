2549f517ad177de80bcba54d14a07f6c74eeb051
scottdiemer <sdiemer@cfl.rr.com>
2019-06-12 22:17:16 -0400

    built Tour and TourList components

---
+++ src/components/TourList/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData";
+
+class TourList extends Component {
+  state = {
+    tours: tourData
+  };
+  removeTour = id => {
+    console.log(id);
+  };
+  render() {
+    const { tours } = this.state;
+
+    return (
+      <section className="tourlist">
+        {tours.map(tour => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default TourList;
